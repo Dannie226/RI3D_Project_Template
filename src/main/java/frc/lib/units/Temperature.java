e1ef98a3ea61e30c7e9eaf2b64f63e9fb1a0241d
Daniel Quednow <danquednow@gmail.com>
2025-10-03 13:12:04 -0400

    Temperature is a special case

--- src/main/java/frc/lib/units/Temperature.java
+++ src/main/java/frc/lib/units/Temperature.java
@@ -1,54 +1,39 @@
 package frc.lib.units;
 
-public class Temperature {
-    public static enum Unit {
+public class Temperature extends Value<Temperature.Unit, Temperature> {
+    public static enum Unit implements frc.lib.units.Unit {
         Celsius,
-        Fahrenheit,
+        Fahrenheit;
+
+        @Override
+        public double getConversionFactor() {
+            return 0;
+        }
     }
 
-    private double celsius;
-
     public Temperature(double temperature, Unit unit) {
+        super(0, unit);
         switch (unit) {
             case Celsius:
-                this.celsius = temperature;
+                this.value = temperature;
                 break;
         
             case Fahrenheit:
-                this.celsius = (temperature - 32.0) * 5.0 / 9.0;
+                this.value = (temperature - 32.0) * 5.0 / 9.0;
                 break;
         }
     }
 
+    @Override
     public double get(Unit unit) {
         switch (unit) {
             case Celsius:
-                return this.celsius;
+                return this.value;
         
             case Fahrenheit:
-                return this.celsius * 9.0 / 5.0 + 32.0;
+                return this.value * 9.0 / 5.0 + 32.0;
         }
 
         throw new NullPointerException();
-    }
-
-    public void add(Temperature t) {
-        this.celsius += t.celsius;
-    }
-
-    public void sub(Temperature t) {
-        this.celsius -= t.celsius;
-    }
-
-    public void mul(double s) {
-        this.celsius *= s;
-    }
-
-    public void div(double s) {
-        this.celsius /= s;
-    }
-
-    public double div(Temperature t) {
-        return this.celsius / t.celsius;
     }
 }
